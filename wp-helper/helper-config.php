f5c77fa53052683d03cd91637038a2cda5efcb07
haoqoo <qintao1203@163.com>
2014-07-28 14:32:32 +0800

    heper-config

---
+++ wp-helper/helper-config.php
@@ -0,0 +1,15 @@
+<?php 
+	/**
+	 * 功能：辅助设置
+	 * 说明：该文件加载到wp-settings.php
+	 */
+
+	// ** 后台左侧菜单 ** //
+	//隐藏更新count
+	define('HIDDEN-UPDATE', true);	
+	//隐藏插件
+	define('HIDDEN-PLUGIN', true);
+	//隐藏工具
+	define('HIDDEN-TOOL', true);
+
+ ?>
